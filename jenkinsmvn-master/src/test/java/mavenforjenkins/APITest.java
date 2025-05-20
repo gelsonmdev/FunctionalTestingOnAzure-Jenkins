45846dded18d6d17ce4455c940c5dd64fb4dc766
Gelson Moreno <gelson-alberto.tavaresmoreno@eviden.com>
2025-05-20 10:45:58 +0100

    first commmit

---
+++ jenkinsmvn-master/src/test/java/mavenforjenkins/APITest.java
@@ -0,0 +1,14 @@
+package mavenforjenkins;
+
+public class APITest {
+
+	
+	public void checkAPIResponse()
+	{
+		System.out.println("Hi");
+                 System.out.println("Good Morning");
+
+	}
+	
+	
+}
